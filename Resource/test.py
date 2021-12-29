6ec7667bb4ff3801df6ef099071e01324942d720
Thanggtrann12 <thanggtrann12@gmail.com>
2021-12-29 13:10:15 +0700

    test

--- Resource/test.py
+++ Resource/test.py
@@ -1,13 +1,26 @@
 import serial
-def portIsUsable(portName):
-    try:
-       ser = serial.Serial(port=portName)
-       return True
-    except:
-       return False
-if portIsUsable('COM1')==True:
-    print("true")
-else:
-    print("False")
 
-b'\xffU\x07\x00\x02\x00\x06'
+import time
+
+serialcomm = serial.Serial('COM8', 115200)
+
+serialcomm.timeout = 1
+
+while True:
+
+    i = input("Enter Input: ").strip()
+
+    if i == "Done":
+
+        print('finished')
+
+        break
+
+    serialcomm.write(i.encode())
+
+    time.sleep(0.5)
+
+    print(serialcomm.readline().decode('ascii'))
+
+serialcomm.close()
+
